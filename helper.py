551f94236a9270e536af0feeddc8efa648495785
nihshu-07 <niharika.tyagi710@gmail.com>
2025-10-18 12:19:28 +0530

    code for media shared

--- helper.py
+++ helper.py
@@ -1,11 +1,14 @@
 def fetch_stats(selected_user,df):
     if selected_user != 'Overall':
         df = df[df['user']== selected_user]
-        
+
     num_messages = df.shape[0]
+
     words=[]
     for message in df['message']:
         words.extend(message.split())
 
-    return num_messages,len(words)
+    num_media_omitted = df[df['message']=='<Media omitted>'].shape[0]
+
+    return num_messages,len(words),num_media_omitted
     
