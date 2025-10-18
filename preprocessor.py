5122088a4c922aee08fa1c5cf078ef5d0f4718ed
nihshu-07 <niharika.tyagi710@gmail.com>
2025-10-18 19:38:49 +0530

    add day_name

--- preprocessor.py
+++ preprocessor.py
@@ -34,6 +34,7 @@ def preprocess(data):
     df['month']=df['message_date'].dt.month_name()
     df['month_num']=df['message_date'].dt.month
     df['day']=df['message_date'].dt.day
+    df['day_name'] = df['message_date'].dt.day_name()
     df['hour'] = df['message_date'].dt.hour
     df['minute'] = df['message_date'].dt.minute
     return df
