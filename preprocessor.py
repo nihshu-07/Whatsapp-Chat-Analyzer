19076f72d18d9ffe08a310f94434de8013266651
nihshu-07 <niharika.tyagi710@gmail.com>
2025-10-19 13:40:03 +0530

    Add period

--- preprocessor.py
+++ preprocessor.py
@@ -37,4 +37,17 @@ def preprocess(data):
     df['day_name'] = df['message_date'].dt.day_name()
     df['hour'] = df['message_date'].dt.hour
     df['minute'] = df['message_date'].dt.minute
+    
+
+    period = []
+    for hour in df[['day_name','hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + '-' + str('00'))
+        elif hour == 0:
+            period.append(str('00') + '-' + str(hour+1))
+        else:
+            period.append(str(hour) + '-' + str(hour+1))
+
+    df['period'] = period
+
     return df
